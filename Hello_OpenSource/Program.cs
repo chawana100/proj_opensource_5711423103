ef8d6cf0c4770fb062d49931223c17be78a4b672
chawana100 <chawana.bain57@sskru.ac.th>
2016-09-09 14:07:42 +0700

    new

---
+++ Hello_OpenSource/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_OpenSource
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello My Name is Chawana ba-in 5711423103");
+
+            Console.WriteLine("Press any key to EXIT.");
+            Console.ReadKey();
+
+        }
+    }
+}
